3a72cf9a63d5a084f38e5fe0aca7850cc1433d93
Danny <ddeneau@terpmail.umd.edu>
2022-03-16 18:33:26 -0400

    Fixed formatting

--- tetris/app.js
+++ tetris/app.js
@@ -13,31 +13,31 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
    const zTetromino = [
-        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
-        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
-        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+     [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+     [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
+     [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+     [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
     ]
     
     const tTetromino = [
-        [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
-        [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
-        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
-        [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
+      [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
+      [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+      [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
     ]
     
     const oTetromino = [
-        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
-        [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+      [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
     ]
     
     const iTetromino = [
-        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
-        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
-        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
-        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
+      [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
+      [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+      [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
     ]    
 
 
