be16a3a72f17fd60f3bf00fdcb82cd492e50b3cb
Danny <ddeneau@terpmail.umd.edu>
2022-04-30 01:58:57 -0400

    Tested submit button after a couple of steps.
    Still works!

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,5 +1,6 @@
 function dataHandler(dataIn) {
-
+  console.log('form submission'); // this is substituting for a "breakpoint"
+  console.table(dataIn); // this is called "dot notation"
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
@@ -7,16 +8,19 @@ async function mainEvent() { // the async keyword means we can make API requests
   const sub = document.querySelector('.button is-primary'); // Get the submit button.
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+
   sub.addEventListener('mouseover', sub.style.setProperty('display: none')); // Change the display style on mouse over.
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+
+  if (length(results) > 0) {
+    sub.addEventListener('mouseover', sub.style.setProperty('block')); // Change the display style on mouse over.
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+    });
+  }
+  dataHandler(arrayFromJson);
 }
 
-dataHandler();
 // this actually runs first! It's calling the function above
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
